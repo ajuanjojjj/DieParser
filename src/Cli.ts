1577ad43cef81451f3ac3cac60ac4ecf9261c72d
Juanjo Martinez <ajuanjojjj@gmail.com>
2026-04-28 17:37:54 +0200

    Move error handler to main function, so args are properly caught and displayed

--- src/Cli.ts
+++ src/Cli.ts
@@ -11,30 +11,30 @@ async function cli() {
 
 	console.log("Enter the dice sequence to parse");
 	const request = await rl.question("> ");
-	try {
-		main(request);
-	} catch (e) {
-		console.error("Error: " + (e as Error).message);
-	}
+	main(request);
 	rl.close();
 }
 
 function main(request: string) {
-	console.log(`Parsing: '${request}'`);
+	try {
+		console.log(`Parsing: '${request}'`);
 
-	const tokens = parse(request);
-	console.log(tokens.stringify());
+		const tokens = parse(request);
+		console.log(tokens.stringify());
 
-	let result1 = simplify(tokens);
-	console.log(stringifyTokens(result1));
+		let result1 = simplify(tokens);
+		console.log(stringifyTokens(result1));
 
-	let result = execute(result1);
-	while (Array.isArray(result)) {
-		console.log(stringifyTokens(result));
-		result = execute(result);
+		let result = execute(result1);
+		while (Array.isArray(result)) {
+			console.log(stringifyTokens(result));
+			result = execute(result);
+		}
+
+		console.log("Result: " + result);
+	} catch (e) {
+		console.error("Error: " + (e as Error).message);
 	}
-
-	console.log("Result: " + result);
 }
 
 const args = process.argv.slice(2);
